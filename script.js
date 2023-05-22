9f7b0e7eefc15584fbaa2229b4daaa229ff7af8a
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2023-05-22 19:08:30 +0200

    add animation

--- script.js
+++ script.js
@@ -59,7 +59,35 @@ form.addEventListener('click', () => {
                 });
             }
         }
-        //validation
+        //name validation
+        input.addEventListener('input', (e) => {
+            if (e.target.type === 'text') {
+                let inputText = e.target.value;
+                if (inputText.length > 2) {
+                    colorize('#6391E8', line, placeholder);
+                } else {
+                    colorize('#FE8C99', line, placeholder);
+                }
+            }
+            //email validation
+            if (e.target.type === 'email') {
+                let valid = validateEmail(e.target.value);
+                if (valid) {
+                    colorize('#6391E8', line, placeholder);
+                } else {
+                    colorize('#FE8C99', line, placeholder);
+                }
+            }
+            //validate phone
+            if (e.target.type === 'tel') {
+                let valid = validatePhone(e.target.value);
+                if (valid) {
+                    colorize('#6391E8', line, placeholder);
+                } else {
+                    colorize('#FE8C99', line, placeholder);
+                }
+            }
+        });
     });
 });
 
@@ -73,3 +101,91 @@ function validatePhone(phone) {
     let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
     return re.test(phone);
 }
+
+//colorize function
+
+function colorize(color, line, placeholder) {
+    gsap.to(line, { stroke: color, duration: 0.75 });
+    gsap.to(placeholder, { color: color, duration: 0.75 });
+}
+
+//checkbox animation fill
+const checkbox = document.querySelector('.checkbox');
+const tl2 = gsap.timeline({
+    defaults: { duration: 0.5, ease: 'Power2.easeOut' },
+});
+
+const tickMarkPath = document.querySelector('.tick-mark path');
+const pathLength = tickMarkPath.getTotalLength();
+
+gsap.set(tickMarkPath, {
+    strokeDashoffset: pathLength,
+    strokeDasharray: pathLength,
+});
+checkbox.addEventListener('click', () => {
+    if (checkbox.checked) {
+        tl2.to('.checkbox-fill', { top: '0%' });
+        tl2.fromTo(
+            tickMarkPath,
+            { strokeDashoffset: pathLength },
+            { strokeDashoffset: 0 },
+            '<50%'
+        );
+        tl.to('.checkbox-label', { color: '#6391e8' });
+    } else {
+        tl2.to('.checkbox-fill', { top: '100%' });
+        tl2.fromTo(
+            tickMarkPath,
+            { strokeDashoffset: 0 },
+            { strokeDashoffset: pathLength },
+            '<50%'
+        );
+        tl.to('.checkbox-label', { color: '#c5c5c5' });
+    }
+});
+
+//character animation
+
+gsap.set('#eye', { transformOrigin: 'center' });
+gsap.fromTo(
+    '#eye',
+    { scaleY: 1 },
+    {
+        scaleY: 0.3,
+        repeat: -1,
+        yoyo: true,
+        repeatDelay: 0.5,
+        ease: 'Power2.easeOut',
+    }
+);
+
+gsap.fromTo(
+    '#eyebrow',
+    { y: 0 },
+    { y: -1, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: 'Power2.easeOut' }
+);
+
+//submit the form
+
+const button = document.querySelector('button');
+const tl3 = gsap.timeline({
+    defaults: { duration: 0.75, ease: 'Power2.easeOut' },
+});
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    tl3.to('.contact-left, .contact-right', {
+        y: 30,
+        opacity: 0,
+        pointerEvents: 'none',
+    });
+    tl3.to('form', { scale: 0.8 }, '<');
+    tl3.fromTo('.submitted', { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
+
+    gsap.set('#hand', { transformOrigin: 'left' });
+    gsap.fromTo(
+        '#hand',
+        { rotation: 0, y: 0 },
+        { rotation: -10, y: 2, ease: 'elastic(3,0.3)', duration: 2, delay: 1 }
+    );
+});
